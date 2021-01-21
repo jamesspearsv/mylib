e11f8b58a82b42f194ae89395891b1ec1e6e4dce
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2021-01-21 11:18:28 -0500

    Update layout template

--- app.py
+++ app.py
@@ -7,7 +7,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///catalog.db"
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    hashword = db.Column(db.String, unique=True, nullable=False)
+
 
 @app.route("/")
 def index():
-    return "TODO"
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        return "TODO"
+    else: 
+        return render_template("register.html")
