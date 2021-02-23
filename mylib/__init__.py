03b0e9b7ca2897be9dd8b294c79a44f8612ca5a3
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2021-02-23 14:17:32 -0500

    Bug fix

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -10,7 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 # Set development mode
-DEV_MODE = True
+DEV_MODE = False
 
 # create flask app
 app = Flask(__name__)
